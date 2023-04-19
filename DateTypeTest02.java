7756695e03c644d19f73053b5e1db8e18f90f152
liuflypen <129945758+liuflypen@users.noreply.github.com>
2023-04-19 18:37:26 +0900

    Add files via upload

---
+++ DateTypeTest02.java
@@ -0,0 +1,17 @@
+*/
+ /*关于八种基本数据类型的默认值
+                              一切向0看齐
+ */
+public class DateTypeTest02
+{
+    static int k = 1000;      //static必须加
+     static int f;    //成员变量  成员变量没有手动赋值系统会默认赋值【局部变量不会】
+     public static void main (String[]args){
+        /*
+        int i;       //局本变量
+        System.out.println(i);
+        */
+       System.out.println(k);
+       String.out.println(f);
+     }
+}
