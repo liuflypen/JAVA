544f2c96dc6acb255c994232bc113a66c12eb3bd
liuflypen <129945758+liuflypen@users.noreply.github.com>
2023-04-19 18:35:16 +0900

    Add files via upload

---
+++ VarTest01.java
@@ -0,0 +1,14 @@
+public class VarTest01
+{
+  public static void main (String[]args){
+    //声明一个int类型的变量，起名1
+    int i;
+    i = 100;
+    //赋值
+    System.out.println(i);
+    i = 200 ;
+    System.out.println(i);
+    //i再次赋值
+    
+  }
+}
